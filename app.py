86131ec9843a6b4fbc32bb71f3b9a34fc744cc58
wanghyscu@163.com <wanghyscu@163.com>
2018-05-07 16:28:26 +0800

    some change

--- app.py
+++ app.py
@@ -12,14 +12,16 @@ def index(request):
 def init(loop):
 	# 定义一个 webAPP;
 	app = web.Application(loop=loop)
-	# 添加 get 方法，router 路由器
+	# 添加 get 方法，router 路由器,并调用上面定义的 index
 	app.router.add_route('GET','/',index)
-	# 用异步协程的方法创建一个网络服务
+	# 用异步协程的方法创建一个网络服务，调用定义的 app
 	srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	# 设置网络登陆信息
 	logging.info('server started at http://127.0.0.1:9000...')
 	return srv
 
-loop = asyncio.get_event_loop()   # 获取异步 IO 的事件循环
+
+# 获取异步 IO 的事件循环
+loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
